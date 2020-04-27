1030583be0d5f58fccb3fc150766f40146616b0c
astn <austin.w.harris@gmail.com>
2020-04-27 02:10:16 -0600

    Some progress on switching to fluent UI

--- poc/UI/Program.cs
+++ poc/UI/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using BlazorFabric;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components;
@@ -18,8 +19,9 @@ namespace UI
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddBlazorFabric();
             builder.RootComponents.Add<App>("app");
-
+            builder.RootComponents.Add<GlobalRules>("#staticcs");
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddSingleton(services =>
