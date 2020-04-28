c6aaf0d7db746790e91342e718db74eb3b54bae2
astn <austin.w.harris@gmail.com>
2020-04-28 15:16:54 -0600

    Show nodes in the grid

---
+++ poc/UI/Pages/GraphHelpers.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Ahghee.Grpc;
+using Microsoft.Extensions.Logging;
+using Antlr4;
+using Antlr4.Runtime;
+using BlazorFabric;
+using cli.antlr;
+using cli_grammer;
+using Grpc.Core.Utils;
+
+namespace UI.Pages
+{
+    public static class GraphHelpers
+    {
+        public static string ToDisplayString(this DataBlock db)
+        {
+            switch (db.DataCase)
+            {
+                case DataBlock.DataOneofCase.None: return "null";
+                case DataBlock.DataOneofCase.Nodeid: return "-> " + db.Nodeid.Iri;
+                case DataBlock.DataOneofCase.Metabytes:return db.Metabytes.Typeiri;
+                case DataBlock.DataOneofCase.Str: return db.Str;
+                case DataBlock.DataOneofCase.I32: return db.I32.ToString();
+                case DataBlock.DataOneofCase.I64: return db.I64.ToString();
+                case DataBlock.DataOneofCase.Ui32: return db.Ui32.ToString();
+                case DataBlock.DataOneofCase.Ui64:return db.Ui64.ToString();
+                case DataBlock.DataOneofCase.D: return db.D.ToString(CultureInfo.CurrentCulture);
+                case DataBlock.DataOneofCase.F: return db.F.ToString(CultureInfo.CurrentCulture);
+                case DataBlock.DataOneofCase.B: return db.B.ToString();
+                case DataBlock.DataOneofCase.Memorypointer: return "(Void*)&pointer";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
